6b0cdbde5573199368661c7e5688e76d22ed5b17
Mahmoud Mezzi <mahmoudmezzi@outlook.fr>
2023-02-23 12:58:51 +0100

    First commit

---
+++ assets/script.js
@@ -0,0 +1,88 @@
+const slides = [
+	{
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+
+const banner = document.getElementById('#banner');
+const left = document.querySelector('.arrow_left');
+const right = document.querySelector('.arrow_right');
+const imageElement = document.querySelector('#banner > img');
+const nomElement = document.querySelector('#banner > p');
+
+const nbr = slides.length;
+
+let currentSlide = 0;
+
+
+
+function dots() {
+	const dots = document.querySelector('.dots');
+	for (let currentSlide = 0; currentSlide < nbr; currentSlide++) {
+		const dot = document.createElement('span');
+		dot.classList.add('dot');
+		dots.appendChild(dot);
+		if (currentSlide == 0) {
+			dots.children[currentSlide].classList.add('dot_selected');
+		}
+	}
+}
+dots();
+
+function selected() {
+	const dot = document.getElementsByClassName('dot');
+	for (let currentSlide = 0; currentSlide < dot.length; currentSlide++) {
+		dot[currentSlide].classList.remove('dot_selected');
+	}
+	dot[currentSlide].classList.add('dot_selected');
+}
+
+function showSlide() {
+	imageElement.src = `./assets/images/slideshow/${slides[currentSlide].image}`;
+	nomElement.innerHTML = slides[currentSlide].tagLine;
+	selected();
+}
+
+left.addEventListener("click", function () {
+	if (currentSlide == 0) {
+		currentSlide = nbr - 1;
+	}
+	else {
+		currentSlide--;
+	}
+	console.log(left);
+	showSlide();
+});
+
+right.addEventListener("click", function () {
+	if (currentSlide == nbr - 1) {
+		currentSlide = 0;
+	} else {
+		currentSlide++;
+	}
+	showSlide();
+
+});
+
+
+
+
+
+
+
+
+
+
